5cf76365e0f63532dc2426ffbb213ab229728054
ma1ing1ing <155398096+ma1ing1ing@users.noreply.github.com>
2024-05-20 00:18:37 +0900

    Create 1.java
    프로그램 기본 구조

---
+++ 2/1.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static int sum(int n, int m){
+        return n+m;
+    }
+    public static void main(String[] args){
+        int i =20;
+        int s;
+        char a='?';
+
+        s = sum(i,10);
+        System.out.print(a);
+        System.out.print("Hello");
+        System.out.print(s);
+    }
+}
