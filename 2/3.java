dda3174bbdb4bcc9d65b4ca645ca7e7f1b788a17
ma1ing1ing <155398096+ma1ing1ing@users.noreply.github.com>
2024-05-20 00:23:16 +0900

    Create 3.java
    변수, 리터럴, 상수 사용

---
+++ 2/3.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        final double pi=3.14;
+        double radius = 10.2;
+        double circleArea = radius*radius*pi;
+
+        System.out.print("반지름"+radius+",");
+        System.out.print("원의 면적"+circleArea);
+    }
+}
