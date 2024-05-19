1a892bd18930db07238dce62515be0a95c4f6a49
ma1ing1ing <155398096+ma1ing1ing@users.noreply.github.com>
2024-05-20 00:23:52 +0900

    3.java

--- 2/3.java
+++ 2/3.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args){
-        final double pi=3.14;
+        static final double pi=3.14;
         double radius = 10.2;
         double circleArea = radius*radius*pi;
 
