6de8265e4eb141cb82300b32696c5e2ff7622259
ma1ing1ing <155398096+ma1ing1ing@users.noreply.github.com>
2024-05-20 00:21:09 +0900

    Create 2.java
    var 사용

---
+++ 2/2.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        var price = 200;
+        var name ="malin";
+        var pi = 3.14;
+
+        System.out.println("price ="+(price+1000));
+        System.out.println("name ="+name);
+        System.out.print("pi"+ pi*10);
+    }
+}
